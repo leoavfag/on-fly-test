6f467a3ca784ec09985287ae78284719213c717c
leoavfag <leoavfag@gmail.com>
2023-04-18 13:09:53 -0300

    feat: add passwordconfirmation check

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   ConflictException,
   InternalServerErrorException,
+  UnprocessableEntityException,
 } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
@@ -18,25 +19,29 @@ export class UsersService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const { email, name, password } = createUserDto
+    const { email, name, password, passwordConfirmation } = createUserDto
 
-    const user = new User()
-    user.email = email
-    user.name = name
-    user.salt = await bcrypt.genSalt()
-    user.password = await this.hashPassword(password, user.salt)
-    try {
-      await this.userRepository.save(user)
-      delete user.password
-      delete user.salt
-      return user
-    } catch (error) {
-      if (error.code.toString() === '23505') {
-        throw new ConflictException('Endereço de email já está em uso')
-      } else {
-        throw new InternalServerErrorException(
-          'Erro ao salvar o usuário no banco de dados',
-        )
+    if (password != passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem')
+    } else {
+      const user = new User()
+      user.email = email
+      user.name = name
+      user.salt = await bcrypt.genSalt()
+      user.password = await this.hashPassword(password, user.salt)
+      try {
+        await this.userRepository.save(user)
+        delete user.password
+        delete user.salt
+        return user
+      } catch (error) {
+        if (error.code.toString() === '23505') {
+          throw new ConflictException('Endereço de email já está em uso')
+        } else {
+          throw new InternalServerErrorException(
+            'Erro ao salvar o usuário no banco de dados',
+          )
+        }
       }
     }
   }
