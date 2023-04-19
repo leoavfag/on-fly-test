d091fa64e57cef3b47a99ed561befcdcf72168e6
leoavfag <leoavfag@gmail.com>
2023-04-19 11:37:53 -0300

    feat: add despesas module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './configs/typeorm.config'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module';
+import { DespesasModule } from './despesas/despesas.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule, DespesasModule],
   controllers: [],
   providers: [],
 })
